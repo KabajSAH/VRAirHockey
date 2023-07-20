5aa8d44b9b9fcc4a6a0d28917b54680f81976071
Math <philipponmathis@gmail.com>
2023-07-20 12:59:26 +0200

    WIP: Scoring Area, Manager and Text

---
+++ Assets/Scripts/Scoring Manager.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ScoringManager : MonoBehaviour
+{
+
+    public List<GameObject> scoringAreas = new List<GameObject>();
+
+    public List<TextMeshPro> scoreTexts;
+    
+    public int score1;
+    public int score2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var scoringArea in scoringAreas)
+        {
+            ScoringArea scoringAreaScript = scoringArea.GetComponent<ScoringArea>();
+            scoringAreaScript.SetScoringManager(this);
+        }
+
+        score1 = 0;
+        score2 = 0;
+    }
+    
+    public void AddScore(int player)
+    {
+        Debug.Log("GOAAAAAAAAAL");
+        if (player == 1) score1++;
+        if (player == 2) score2++;
+        
+        UpdateScoreTexts();
+    }
+    
+    private void UpdateScoreTexts()
+    {
+        scoreTexts[0].text = score1 + "  -  " + score2;
+        scoreTexts[1].text = score2 + "  -  " + score1;
+    }
+}
